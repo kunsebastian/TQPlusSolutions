99f7275af931d67a8b51cda5a008d71845b3a87b
Kunle Badeafuye <kunle.xceed365@gmail.com>
2019-04-17 11:01:51 +0100

    swagger config

---
+++ TQPlusSolutionsDM/Scripts/swagger-bearer-auth.js
@@ -0,0 +1,21 @@
+﻿$(function () {
+    var tokenAuthUI = '<div class="input"><input placeholder="token here" id="input_token" name="token" type="text" size="25"></div>';
+
+    $(tokenAuthUI).insertBefore('#api_selector div.input:last-child');
+
+    $("#input_apiKey").hide();
+
+    $('#input_token').change(addAuthorization);
+
+    function addAuthorization() {
+        var token = encodeURIComponent($('#input_token').val());
+
+        if (token && token.trim() !== "") {
+            var apiKeyAuth = new SwaggerClient.ApiKeyAuthorization("Authorization", "Bearer " + token, "header");
+
+            window.swaggerUi.api.clientAuthorizations.add("bearer", apiKeyAuth);
+
+            console.log("Bearer token authorization added!");
+        }
+    }
+});
